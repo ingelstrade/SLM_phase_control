4a59a2115621352cf876a2eaa8d907bebc33ba41
Marvin Schmoll <ma4871sc@fys.local>
2022-08-26 10:55:59 +0200

    Escape key closes publication window

--- controlPhaseSLM.py
+++ controlPhaseSLM.py
@@ -2,6 +2,7 @@ from settings import SANTEC_SLM, slm_size, bit_depth
 import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
+import tkinter.messagebox  as tkMbox
 import numpy as np
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 import json
@@ -101,6 +102,8 @@ class main_screen(object):
         self.main_win.bind('w', uphandler)
         def downhandler(event): return self.down_arrow()
         self.main_win.bind('s', downhandler)
+        def escapehandler(event): return self.escape_key()
+        self.main_win.bind('<Escape>', escapehandler)
 
         # loading last settings
         self.load('./last_settings.txt')
@@ -404,6 +407,15 @@ class main_screen(object):
             self.phase_refs[2].down_()
             self.open_pub()
             self.main_win.after(500, self.main_win.focus_force)
+    
+    def escape_key(self):
+        if self.pub_win is not None:
+            q_str = 'Do you want to close the SLM Publication Window?\nThe SLM screen will instead show the desktop background.'
+            result = tkMbox.askquestion('Close Publication Window', q_str)
+            if result == 'yes':
+                if not SANTEC_SLM:
+                    self.pub_win.win.destroy()
+                self.pub_win_closed()
 
     def update_phase_plot(self, phase):
         self.ax1.clear()
